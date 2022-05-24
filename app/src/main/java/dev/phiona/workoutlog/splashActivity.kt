fff40944e751a701808f8d6ae76cee8957881493
ephy-phiona <ephyphiona@gmail.com>
2022-05-24 20:13:04 +0300

    first commit

---
+++ app/src/main/java/dev/phiona/workoutlog/splashActivity.kt
@@ -0,0 +1,14 @@
+package dev.phiona.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
