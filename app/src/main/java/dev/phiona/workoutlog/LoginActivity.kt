33385e88d7cc1e2aa05bec9de3b8047f53927c64
ephy-phiona <ephyphiona@gmail.com>
2022-05-31 20:15:13 +0300

    first commit

--- app/src/main/java/dev/phiona/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/phiona/workoutlog/LoginActivity.kt
@@ -1,11 +1,21 @@
 package dev.phiona.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var tvSignUp: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        tvSignUp=findViewById(R.id.tvSignup)
+        tvSignUp.setOnClickListener{
+            val  intent=Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
